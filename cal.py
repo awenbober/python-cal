b617fe28b2b09e4cb403747a14e8f297311c14da
Misco <kmtsandeepanie@gmail.com>
2020-10-21 00:28:20 +0530

    Create cal

--- cal.py
+++ cal.py
@@ -1,1 +1,11 @@
+class Calculator:
+	def add(self, firstNum, secondNum):
+		return firstNum + secondNum
+	
 
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+myCalculator = Calculator()
+sum = myCalculator.add(a,b)
+print("Sum is: ",sum)
