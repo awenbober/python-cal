5413b9d250cf0e44dcc62a6a3fc9383313cbf2e5
Misco <kmtsandeepanie@gmail.com>
2020-10-22 00:08:58 +0530

    Add subtraction

--- cal.py
+++ cal.py
@@ -1,6 +1,8 @@
 class Calculator:
 	def add(self, firstNum, secondNum):
 		return firstNum + secondNum
+	def subtract(self, firstNum, secondNum):
+		return firstNum - secondNum
 	
 
 a = int(input("enter first number: "))
@@ -8,4 +10,7 @@ b = int(input("enter second number: "))
 
 myCalculator = Calculator()
 sum = myCalculator.add(a,b)
-print("Sum is: ",sum)
+print("Result of addition: ",sum)
+
+sub = myCalculator.subtract(a,b)
+print("Result of subtraction: ",sub)
