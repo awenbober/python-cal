3db739243a5c9b111f6922906b381777b9ff85e4
Salha Salman <salhasalman4@gmail.com>
2020-10-29 21:54:05 +0530

    added base of log

--- cal.py
+++ cal.py
@@ -13,8 +13,10 @@ class Calculator:
 		return pow(firstNum, secondNum)
 	def squareRoot(self,mul):
 		return math.sqrt(mul)
-	def logarithm(self,firstNum):
+	def logarithm1(self,firstNum):
 	    return math.log(firstNum)
+	def logarithm2(self,firstNum,secondNum):
+		return math.log(firstNum,secondNum)
 
 a = int(input("enter first number: "))
 b = int(input("enter second number: "))
@@ -39,5 +41,8 @@ squareRoot=myCalculator.squareRoot(mul)
 print("Result of squareRoot",squareRoot)
 
 #Printing the log base e of input number
-logarithm=myCalculator.logarithm(a)
-print("Result of logarithm",logarithm)
+logarithm1=myCalculator.logarithm1(a)
+print("Result of logarithm",logarithm1)
+
+logarithm2=myCalculator.logarithm2(a,b)
+print("Result of logarithm",logarithm2)
